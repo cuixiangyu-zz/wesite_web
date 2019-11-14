973ba4bc30499b3028bb6734871cc75b00bcdbf0
cuixiangyu-zz <Cuixiangyu1995@163.com>
2019-11-14 16:58:04 +0800

    aaa

--- src/views/video_detail/index.vue
+++ src/views/video_detail/index.vue
@@ -85,6 +85,34 @@ export default {
     getDetil() {
       this.querylist.id = this.$route.params.id
       console.log(this.querylist)
+
+        getDetil(this.querylist).then(res =>    {
+            console.log(res)
+            this.tableData = res
+            for (const i of this.tableData.address) {
+                const videoinfo = {
+                    playbackRates: [0.7, 1.0, 1.5, 2.0], // 播放速度
+                    autoplay: false, // 如果true,浏览器准备好时开始回放。
+                    controls: true, // 控制条
+                    preload: 'auto', // 视频预加载
+                    muted: false, // 默认情况下将会消除任何音频。
+                    loop: false, // 导致视频一结束就重新开始。
+                    language: 'zh-CN',
+                    aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+                    fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+                    sources: [
+                        {
+                            src: i
+                        }
+                    ],
+                    poster: this.tableData.coverUrl,
+                    width: document.documentElement.clientWidth,
+                    notSupportedMessage: '此视频暂无法播放，请稍后再试' // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
+                }
+                this.srcList.push(videoinfo)
+            }
+            console.log(this.srcList)
+        })
     },
 
     handleClick() {
